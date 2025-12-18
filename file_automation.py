dff330509adefef9eb9e75c8172777c013865a95
matthewmonn-gh <matthewm002@gmail.com>
2025-12-18 11:48:30 -0800

    Adjusted program to use pathlib instead of shutil. Added function check_directory_is_valid

--- file_automation.py
+++ file_automation.py
@@ -1,39 +1,51 @@
-"""
-3rd version of the program. Switch from using shutil to pathlib. Reason being there is an 
-issue with shutil when moving a file that 
-"""
-
 # Provides a way for file and directory operations, such as copying and moving.
-import shutil
+from pathlib import Path
 
-# Hardcoded file to be moved and destination location
-source_file = "C:/Users/13605/Downloads/MatthewMonnResume.pdf"
-destination_location = "C:/Users/13605/Desktop/DL_Docs/"
-
-
-# Potential issues that can occur:
-# 1. File is missing from 'source_file'
-# 2. Destination does not exist. Should it be created or just thrown an exception? This version will throw an error.
-#       Without an error source file is 'destroyed' by becoming a simple file.
-# 3. Permission issues with accessing file or destination
+# Hard coded file name and source/destination directories. Future update might change this to dynamic or more than 1 file.
+source_file = "MatthewMonnResume.pdf"
+source_directory = Path("C:/Users/13605/Downloads/")
+destination_directory = Path("C:/Users/13605/Desktop/DL_Docs/")
 
 
-def move_file(file, location):
-    """Moves a file from one location to another
+def check_directory_is_valid(directory):
+    """Checks if a directory exists or not
 
     Args:
-        file (string): File to be moved
-        location (string): Location for file to be moved to
+        directory (string): Path of a directory
+
+    Returns:
+        boolean: Provides True or False if directory exists
     """
-    try:
-        shutil.move(source_file, destination_location)
-        print("File was successfully moved ")
-    except FileNotFoundError:
-        print("Source file does not exist")
-    except PermissionError as e:
-        print("Permissions error has occured. Please adjust permission settings for file/destination and that file is closed")
-    except Exception as e:
-        print(f"Unknown error has occured: {e}")
+    if directory.is_dir():
+        return True
+    else:
+        print("Directory of where the file is to be moved does not exist. Please create the directory first, then try again.")
+        return False
 
 
-move_file(source_file, destination_location)
+def move_file(source_file: str, source_directory: Path, destination_directory: Path):
+    """Moves a file from one directory to another if possible
+
+    Args:
+        source_file (str): Name of file to be moved
+        source_directory (Path): Path of the directory that contains the source file
+        destination_directory (Path): Path of the directory that the file is to be moved to
+    """
+    if check_directory_is_valid(destination_directory):
+        source_full_path = source_directory / source_file
+        destination_full_path = destination_directory / source_file
+
+        try:
+            source_full_path.rename(destination_full_path)
+            print(
+                f"Moved file '{source_file}' successfully to '{destination_directory}'.")
+        except FileNotFoundError:
+            print(
+                f"Error: Source file '{source_file}' does not exist in directory '{source_directory}'.")
+        except PermissionError as e:
+            print("Permission error has occured. Please adjust permission settings for file/destination and that file is closed.")
+        except Exception as e:
+            print(f"Unknown error has occured: {e}")
+
+
+move_file(source_file, source_directory, destination_directory)
